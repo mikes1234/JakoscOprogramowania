f879b3ad24518a62429cec7bb8b64c4aaa3d8724
Mikes <Mikes@DESKTOP-D1QFCJS>
2019-04-19 17:24:51 +0200

    Pierwszy commit

---
+++ Jakosc_Oprogramowania/src/main/java/com/Projekt/Jakosc_Oprogramowania/Start.java
@@ -0,0 +1,13 @@
+package com.Projekt.Jakosc_Oprogramowania;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Start {
+
+	@RequestMapping("/czesc")
+	public String czesc() {
+		return "Cześć";
+	}
+}
