441f6ff33a4ebb093f9b0103224db92053949172
Mikes <Mikes@DESKTOP-D1QFCJS>
2019-05-31 15:29:38 +0200

    Selenium Update

--- Jakosc_Oprogramowania/src/test/java/com/reservationservicetest/SeleniumTest.java
+++ Jakosc_Oprogramowania/src/test/java/com/reservationservicetest/SeleniumTest.java
@@ -23,54 +23,80 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SeleniumTest {
 
 	@Test
-    public void seleniumTest1(){
-    	System.setProperty("webdriver.edge.driver", "C:/Users/Mikęs/Desktop/MicrosoftWebDriver.exe");
-        WebDriver driver = new EdgeDriver();
-        
-        driver.get("http://localhost:8080/");
-        
-        driver.findElement(By.linkText("Create new reservation")).click();
-        WebElement myDynamicElement = 
-        		(new WebDriverWait(driver, 100)).until(ExpectedConditions.presenceOfElementLocated(By.linkText("Create new reservation")));
-        driver.findElement(By.id("resObject")).sendKeys("Table10");
-        driver.findElement(By.id("firstName")).sendKeys("Adrian");
-        driver.findElement(By.id("lastName")).sendKeys("Nowak");
-        driver.findElement(By.id("resDate")).sendKeys("300620191010");
-       
-        myDynamicElement = 
-        		(new WebDriverWait(driver, 100)).until(ExpectedConditions.presenceOfElementLocated(By.id("submit")));
-        
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.id("submit")).click();
+	public void seleniumTestPositive() throws InterruptedException {
+		System.setProperty("webdriver.edge.driver", "C:/Users/Mikęs/Desktop/MicrosoftWebDriver.exe");
+		WebDriver driver = new EdgeDriver();
 
-        
-        driver.quit();
-       
-    }
+		driver.get("http://localhost:8080/");
+
+		driver.findElement(By.linkText("Create new reservation")).click();
+		WebElement myDynamicElement = (new WebDriverWait(driver, 100))
+				.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Create new reservation")));
+		driver.findElement(By.id("resObject")).sendKeys("Table10");
+		driver.findElement(By.id("firstName")).sendKeys("Adrian");
+		driver.findElement(By.id("lastName")).sendKeys("Nowak");
+		driver.findElement(By.id("resDate")).sendKeys("300620191010");
+
+	//	myDynamicElement = (new WebDriverWait(driver, 100))
+		//		.until(ExpectedConditions.presenceOfElementLocated(By.id("submit")));
+
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		Thread.sleep(2500);
+		driver.findElement(By.id("submit")).click();
+
+		driver.quit();
+
+	}
+
 	@Test
-    public void seleniumTest2(){
-    	System.setProperty("webdriver.edge.driver", "C:/Users/Mikęs/Desktop/MicrosoftWebDriver.exe");
-        WebDriver driver = new EdgeDriver();
-        
-        driver.get("http://localhost:8080/");
-        
-        driver.findElement(By.linkText("Reservations")).click();
-        
-        long end = System.currentTimeMillis() + 5000;
+	public void seleniumTestDisplyingReservations() {
+		System.setProperty("webdriver.edge.driver", "C:/Users/Mikęs/Desktop/MicrosoftWebDriver.exe");
+		WebDriver driver = new EdgeDriver();
 
-        while(System.currentTimeMillis() < end){
-            ArrayList<WebElement> resultDiv = (ArrayList<WebElement>) driver.findElements(By.className("sbsb_a"));
-            if(resultDiv.size() > 0) {
-                break;
-            }
-        }
-        List<WebElement> allSugestions = driver.findElements(By.className("data"));
+		driver.get("http://localhost:8080/");
 
-        for(WebElement suggestions : allSugestions){
-            System.out.println(suggestions.getText());
-        }
+		driver.findElement(By.linkText("Reservations")).click();
 
-        System.out.println(driver.getTitle());
-        //driver.quit();
+		long end = System.currentTimeMillis() + 5000;
+
+		while (System.currentTimeMillis() < end) {
+			ArrayList<WebElement> resultDiv = (ArrayList<WebElement>) driver.findElements(By.className("sbsb_a"));
+			if (resultDiv.size() > 0) {
+				break;
+			}
+		}
+		List<WebElement> allSugestions = driver.findElements(By.className("data"));
+
+		for (WebElement suggestions : allSugestions) {
+			System.out.println(suggestions.getText());
+		}
+
+		System.out.println(driver.getTitle());
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.quit();
+	}
+
+	@Test
+	public void seleniumTestNegative() throws InterruptedException {
+		System.setProperty("webdriver.edge.driver", "C:/Users/Mikęs/Desktop/MicrosoftWebDriver.exe");
+		WebDriver driver = new EdgeDriver();
+
+		driver.get("http://localhost:8080/");
+
+		driver.findElement(By.linkText("Create new reservation")).click();
+		WebElement myDynamicElement = (new WebDriverWait(driver, 100))
+				.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Create new reservation")));
+		driver.findElement(By.id("resObject")).sendKeys("@#$$");
+		driver.findElement(By.id("firstName")).sendKeys("!#@#!@#!@#");
+		driver.findElement(By.id("lastName")).sendKeys(".........");
+
+		myDynamicElement = (new WebDriverWait(driver, 100))
+				.until(ExpectedConditions.presenceOfElementLocated(By.id("submit")));
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.id("submit")).click();
+		Thread.sleep(4000);
+		driver.quit();
 	}
 }
